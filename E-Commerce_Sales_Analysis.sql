708f05be6fabe1d29ff513c3c4b3930e1149d133
yogendragabhane <yogendra.gabhane1@gmail.com>
2025-10-21 12:37:07 +0530

    Update E-Commerce_Sales_Analysis.sql

--- E-Commerce_Sales_Analysis.sql
+++ E-Commerce_Sales_Analysis.sql
@@ -1,4 +1,10 @@
 sql
 
-  -- Identify the top 3 cities with the highest number of customers to determine key markets for targeted marketing and logistic optimization.
+-- Identify the top 3 cities with the highest number of customers to determine key markets for targeted marketing and logistic optimization.
+
+Select location,Count(customer_id) as number_of_customers
+From customers
+Group By location
+Order By Count(customer_id) Desc
+Limit 3;
 
